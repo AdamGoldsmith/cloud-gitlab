ee02472cb877007f6a663af48cbc94ec80a8fb2c
Adam Goldsmith <adam.goldsmith75@gmail.com>
2020-06-15 14:32:24 +0100

    Convert GitLab deployment to terraform

---
+++ terraform/gcp/gitlab/main.tf
@@ -0,0 +1,76 @@
+provider "google" {
+  credentials = file(var.cred_file)
+  project     = var.project_name
+  region      = var.region_name
+}
+
+data "google_compute_network" "project_network" {
+  name = "${var.network_name}-network"
+}
+
+data "google_compute_image" "centos_image" {
+  family  = var.os_family_name
+  project = var.os_project_name
+}
+
+data "google_compute_address" "service_address" {
+  name = "service-1-address"
+}
+
+resource "google_compute_firewall" "allow_gitlab" {
+  name          = "allow-gitlab"
+  network       = data.google_compute_network.project_network.self_link
+  description   = "GitLab service port"
+  priority      = 65500
+
+  allow {
+    protocol = "tcp"
+    ports    = ["4483"]
+  }
+}
+
+resource "google_compute_instance" "gitlab_instance" {
+  name         = "${var.service_name}-${random_pet.server_name.id}"
+  machine_type = var.machine_type
+  zone         = var.instance_zone
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.centos_image.self_link
+      size  = var.disk_size
+    }
+    auto_delete = true
+  }
+  network_interface {
+    network    = data.google_compute_network.project_network.self_link
+    # access_config {
+    #   nat_ip = google_compute_address.service_address.address
+    #   network_tier = "STANDARD"
+    # }
+  }
+  labels = {
+    ansible_group = var.service_name
+  }
+  tags = [var.service_name]
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${file(var.ssh_pubkey)}"
+  }
+}
+
+resource "google_compute_target_pool" "gitlab_target_pool" {
+  name   = "${var.service_name}-1-target-pool"
+  instances = [
+    "${var.instance_zone}/${google_compute_instance.gitlab_instance.name}"
+  ]
+}
+
+resource "google_compute_forwarding_rule" "gitlab_forwarding_rule" {
+  name                  = "${var.service_name}-1-forwarding-rule"
+  load_balancing_scheme = "EXTERNAL"
+  # TODO: Parameterise network_tier
+  network_tier          = "STANDARD"
+  target                = google_compute_target_pool.gitlab_target_pool.self_link
+  ip_protocol           = var.ip_protocol
+  port_range            = var.port_range
+  ip_address            = data.google_compute_address.service_address.address
+}
