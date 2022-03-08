3cef547a2fc2e036d84cf169f2c505afedc75df4
Adam Goldsmith <adam.goldsmith75@gmail.com>
2022-03-08 09:12:36 +0000

    Remove GCP terraform directory

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "gcs" {
-    # Change bucket name here!
-    bucket = "ks-gitlab-tf-backend"
-    prefix = "terraform/state"
-  }
-}
--- terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "service_account_email" {
-  type        = string
-  description = "Email for service account"
-}
